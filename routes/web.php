eeca995139d6667d8019aee22d77d5309d42a182
adrimore30 <caradrimocas31@gmail.com>
2025-04-18 01:15:12 -0500

    1  cambio adri falta serve

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
-
+use App\Http\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::Controller(UserController::class)->group(function () {
+    Route::get('usse','enviar');
+    Route::post('usse/env','enviarUsuario');
+
 });
