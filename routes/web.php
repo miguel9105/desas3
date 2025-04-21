97f966ad3f59e701ae5b94091ee9989a65d8f892
MichelleJimenez01 <alba_mjimenez@soy.sena.edu.co>
2025-04-21 17:14:48 -0500

    ingreso de modelo y formulario message2

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Controllers;
-use App\Models\Message;
-use Illuminate\Support\Facades\Route;
-
-Route::controller(MessageController::class)->group(function ()  {
-    Route::get('vistamensaje','enviar');
-    Route::post('vistamensaje','store')->name('Message.store');
-});
