63f05a5703836fdffdffbf9e2dc50cb9c8b510c0
MichelleJimenez01 <alba_mjimenez@soy.sena.edu.co>
2025-04-21 11:12:07 -0500

    actualizacion en modelo y controlador de Message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Message;
+//use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    // la funcion index devuelve todos los mensajes con su respectivo rol
+    public function index()
+    {
+        // trae todos los mensajes junto con su relacion role
+        //on with se puede traer todo en unsa solaconsulta sin este se utilizaria lazy loaiding el cual es mas largo y cargaria mas el sistema 
+        $mensajes = Message::with('role')->get();
+
+        // devuelve los datos en formato JSON
+        return response()->json($mensajes);
+    }
+
+    //la funcion show muestra un mensaje especifico por su id
+    public function show($id)
+    {
+        // busca el mensaje por id, y tambien trae su rol relacionado esto es posible ya q utilizamos findOrFail si no existe el id lanza un error404
+        $mensaje = Message::with('role')->findOrFail($id);
+
+        
+        return response()->json($mensaje);
+    }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id', 'role_id');
+    }
 }
