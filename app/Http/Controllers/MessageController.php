6160bf5efb0f7d9fd02d81dea13eccfe5f39b644
MichelleJimenez01 <alba_mjimenez@soy.sena.edu.co>
2025-04-21 17:03:16 -0500

    ingreso de modelo y formulario message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,28 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Message;
-//use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    // la funcion index devuelve todos los mensajes con su respectivo rol
-    public function index()
+    // Mostrar el formulario para enviar un mensaje
+    public function enviar()
     {
-        // trae todos los mensajes junto con su relacion role
-        //on with se puede traer todo en unsa solaconsulta sin este se utilizaria lazy loaiding el cual es mas largo y cargaria mas el sistema 
-        $mensajes = Message::with('role')->get();
-
-        // devuelve los datos en formato JSON
-        return response()->json($mensajes);
+        return view('createMessage');
     }
 
-    //la funcion show muestra un mensaje especifico por su id
-    public function show($id)
-    {
-        // busca el mensaje por id, y tambien trae su rol relacionado esto es posible ya q utilizamos findOrFail si no existe el id lanza un error404
-        $mensaje = Message::with('role')->findOrFail($id);
-
-        
-        return response()->json($mensaje);
+    public function store(Request $request){
+        $respu= new Message();
+        $respu->content=$request->content;
+        $respu->is_admin_message=$request->is_admin_message;
+        $respu->is_read=$request->is_read;
+        $respu->save();
+        return $respu;
     }
 }
--- database/migrations/2025_04_18_014559_create_messages_table.php
+++ database/migrations/2025_04_18_014559_create_messages_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,13 +15,12 @@ return new class extends Migration
             $table->string('content');
             $table->string('is_admin_message');
             $table->string('is_read');
-            $table->unsignedBigInteger('role_id'); // Relacion con la tabla roles
-            $table->timestamps();
-
+            $table->unsignedBigInteger('role_id') ->nullable(); // Relacion con la tabla roles
             $table->foreign('role_id')
-            ->references('role_id')
+            ->references('id')
             ->on('roles')
             ->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
