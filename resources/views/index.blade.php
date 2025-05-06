cd3f0e694273217a33b50aa8ddd2b3cfcf4ad915
desoff2026 <luismiguel200365@gmail.com>
2025-05-06 00:27:46 -0500

    correccion de navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,20 @@
 @section('contend')
     
 <!-- About-->
+</nav>
+ 
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+        <div class="d-flex justify-content-center">
+            <div class="text-center">
+                <h1 class="mx-auto my-0 text-uppercase">Desas3</h1>
+                <h2 class="text-white-50 mx-auto mt-2 mb-5">la informacion correcta en el momento correcto.</h2>
+                <a class="btn btn-primary" href="#about">Get Started</a>
+            </div>
+        </div>
+    </div>
+</header>
 
         <section class="about-section text-center" id="about">
             <div class="container px-4 px-lg-5">
